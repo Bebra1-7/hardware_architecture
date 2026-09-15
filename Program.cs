7aeff66c9634c8a24e80b032b5bb84a24856583f
Bebra1-7 <grigorievn603@gmail.com>
2026-09-15 21:51:45 +0300

    Lab01: System monitor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
+﻿// Console.WriteLine($"Компьютер: {Environment.MachineName}");
+// Console.WriteLine($"User: {Environment.UserName}");
+// Console.WriteLine($"Date and time: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+// Console.WriteLine($"OS: {Environment.OSVersion}");
+// Console.WriteLine($"64-bit OS: {Environment.Is64BitOperatingSystem}");
+
+// Console.WriteLine($"Logic Procces: {Environment.ProcessorCount}");
+
+// Console.WriteLine($"PID CPU: {Environment.ProcessId}");
+// long memory = Environment.WorkingSet / 1024 / 1024;
+// Console.WriteLine($"ОЗУ: {memory} MB");
+// Console.ReadLine();
+
+
+using System.Diagnostics;
+
+Console.WriteLine($"Компьютер: {Environment.MachineName}");
 Console.WriteLine($"User: {Environment.UserName}");
 Console.WriteLine($"Date and time: {DateTime.Now:dd.MM.yyyy HH:mm}");
 
@@ -7,7 +24,20 @@ Console.WriteLine($"64-bit OS: {Environment.Is64BitOperatingSystem}");
 
 Console.WriteLine($"Logic Procces: {Environment.ProcessorCount}");
 
-Console.WriteLine($"PID CPU: {Environment.ProcessId}");
-long memory = Environment.WorkingSet / 1024 / 1024;
-Console.WriteLine($"ОЗУ: {memory} MB");
-Console.ReadLine();
+Process currentProcess = Process.GetCurrentProcess();
+Console.WriteLine($"PID CPU: {currentProcess.Id}\n");
+
+Console.WriteLine($"ОЗУ process:");
+
+for (int i = 1; i <= 3; i++)
+{
+    currentProcess.Refresh();
+    long memory = currentProcess.WorkingSet64 / 1024 / 1024;
+    Console.WriteLine($"{i} измерения: {memory} MB");
+    if (i < 3)
+    {
+        Console.WriteLine($"Обновите окно через несколько секунд...");
+        Console.ReadLine();
+    }
+}
+Console.WriteLine("Исследование завершено.");
