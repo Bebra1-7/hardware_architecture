f9171f16afe0e41aaec95a961b68f1045ac64f3d
Bebra1-7 <grigorievn603@gmail.com>
2026-09-15 21:35:10 +0300

    Lab01: report

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
+Console.WriteLine($"User: {Environment.UserName}");
+Console.WriteLine($"Date and time: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+Console.WriteLine($"OS: {Environment.OSVersion}");
+Console.WriteLine($"64-bit OS: {Environment.Is64BitOperatingSystem}");
+
+Console.WriteLine($"Logic Procces: {Environment.ProcessorCount}");
+
+Console.WriteLine($"PID CPU: {Environment.ProcessId}");
+long memory = Environment.WorkingSet / 1024 / 1024;
+Console.WriteLine($"ОЗУ: {memory} MB");
+Console.ReadLine();
